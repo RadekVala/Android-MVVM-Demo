ba6df7066a9f63438b90a8815d8a5bd8b56cc57f
Radek Vala <radek.www@gmail.com>
2022-10-26 15:42:45 +0200

    Login Event handler created in ViewModel

--- app/src/main/java/cz/utb/fai/mvvmdemo/viewmodels/LoginViewModel.kt
+++ app/src/main/java/cz/utb/fai/mvvmdemo/viewmodels/LoginViewModel.kt
@@ -1,7 +1,13 @@
 package cz.utb.fai.mvvmdemo.viewmodels
 
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+
+import cz.utb.fai.mvvmdemo.R
 import cz.utb.fai.mvvmdemo.models.User
 
 class LoginViewModel : ViewModel() {
@@ -16,5 +22,12 @@ class LoginViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("MYAPP", "LoginViewModel was destroyed!")
+    }
+
+    fun btnLoginClicked (view: View) {
+        Log.i("MYAPP", "Button Login was clicked")
+        if(user.email == "admin@example.com" && user.password == "123456789") {
+            view.findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
     }
 }
